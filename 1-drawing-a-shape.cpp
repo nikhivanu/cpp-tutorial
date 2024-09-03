d7d0667dc4f53d702e0d89c3ec12d2000055b140
Nikhita Anusuri <nikhi.v.anu@gmail.com>
2024-09-03 08:43:42 -0400

    Add drawing a shape cpp file

---
+++ 1-drawing-a-shape.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
+}
