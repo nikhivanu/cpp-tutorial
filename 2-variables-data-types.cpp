b49f331c6ed24a2545ee54fcb73d31f6cf506d48
Nikhita Anusuri <nikhi.v.anu@gmail.com>
2024-09-03 08:51:56 -0400

    Create variables-data-types

---
+++ 2-variables-data-types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    string characterName = "Tom";
+    int characterAge;
+    characterAge = 35;
+
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    characterName = "Mike";
+    cout << "He liked the name "<< characterName << endl;
+    cout << "But did not like being " << characterAge << endl;
+    
+
+    char grade = 'A';
+    string phrase = "Giraffe Academy";
+    int age = -50;
+    double gpa = 2.3;
+    bool isMale = false;
+    cout << grade << endl;
+}
