c207f47abeb0660ea60ffc902df5a86abf58354c
Nikhita Anusuri <nikhi.v.anu@gmail.com>
2024-09-07 21:24:43 -0400

    create arrays

---
+++ 8-arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+int main(){
+    int luckyNums [] = {3, 12, 4, 34, 65, 23};
+
+    cout << luckyNums[0];
+    cout << luckyNums[5];
+
+    int luckyNums2[20];
+    luckyNums2[0] = 100;
+}
