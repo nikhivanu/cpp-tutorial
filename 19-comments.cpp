9d53ac68d80b8525b7c3384ee50ee622a0b79daf
Nikhita Anusuri <nikhi.v.anu@gmail.com>
2024-09-08 08:37:24 -0400

    create comments

---
+++ 19-comments.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //prints out Comments are fun
+    /*
+    for comments that take 
+    up multiple lines */
+    cout << "Comments are fun" << endl;
+    return 0; 
+}
