144ad1a7651f8e0c40447701b503fb382e8d04e9
Nikhita Anusuri <nikhi.v.anu@gmail.com>
2024-09-08 08:36:29 -0400

    create for loops

---
+++ 16-for-loops.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int nums[] = {1, 2, 3, 4, 5, 6};
+
+    for(int i = 0; i < 5; i++){
+        cout << nums[i] << endl;
+    }
+    return 0;
+}
